eab2fb8b4972f46130415080f1e27f282d4ab64c
Matias Gomez <jmgametal@gmail.com>
2019-02-01 10:50:05 -0300

    preparacion del proyecto con firebase y bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDcUSAoXjCOpmUwNHD2Yg4xOcubsDoPjjY',
+    authDomain: 'oshop-c448f.firebaseapp.com',
+    databaseURL: 'https://oshop-c448f.firebaseio.com',
+    projectId: 'oshop-c448f',
+    storageBucket: 'oshop-c448f.appspot.com',
+    messagingSenderId: '771176054519'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDcUSAoXjCOpmUwNHD2Yg4xOcubsDoPjjY',
+    authDomain: 'oshop-c448f.firebaseapp.com',
+    databaseURL: 'https://oshop-c448f.firebaseio.com',
+    projectId: 'oshop-c448f',
+    storageBucket: 'oshop-c448f.appspot.com',
+    messagingSenderId: '771176054519'
+  }
 };
 
 /*
