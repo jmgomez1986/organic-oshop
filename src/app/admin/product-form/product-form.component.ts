749999121d21950a1b63f81b2ff11a64ec43e4a9
Matias Gomez <jmgametal@gmail.com>
2019-02-07 10:54:26 -0300

    Se implementa el borrado de productos

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -24,15 +24,22 @@ export class ProductFormComponent implements OnInit {
 
     this.id = this.route.snapshot.paramMap.get('id');
     if (this.id) {
-      this.productService.getProduct(this.id).take(1).subscribe(p => this.product = p); // take(1) termina la subcripcion de un elemento
+      this.productService.get(this.id).take(1).subscribe(p => this.product = p); // take(1) termina la subcripcion de un elemento
     }
   }
 
   save(product: any) {
     if (this.id) {
-      this.productService.updateProduct(this.id, product);
+      this.productService.update(this.id, product);
     } else { this.productService.create(product); }
 
+    this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) { return; }
+
+    this.productService.delete(this.id);
     this.router.navigate(['/admin/products']);
   }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,11 +19,15 @@ export class ProductService {
     });
   }
 
-  getProduct(productId: string) {
+  get(productId: string) {
     return this.db.object('/products/' + productId).valueChanges();
   }
 
-  updateProduct(productId: string, product: any) {
+  update(productId: string, product: any) {
     return this.db.object('/products/' + productId).update(product);
+  }
+
+  delete(productId: string) {
+    return this.db.object('/products/' + productId).remove();
   }
 }
