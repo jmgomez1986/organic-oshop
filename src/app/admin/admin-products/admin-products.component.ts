471827c005a79b4656f2ce23108584d7ca569b23
Matias Gomez <jmgametal@gmail.com>
2019-02-07 12:29:24 -0300

    Se implementa foltrado de productos y se refactoriza creando una interfaz para el arreglo de productos

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/product.service';
-import { Observable } from 'rxjs/Observable';
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { ProductService } from 'src/app/product.service';
+// import { Observable } from 'rxjs/Observable';
+// import { Subscription } from 'rxjs';
+//
+// @Component({
+//   selector: 'app-admin-products',
+//   templateUrl: './admin-products.component.html',
+//   styleUrls: ['./admin-products.component.css']
+// })
+// export class AdminProductsComponent implements OnInit, OnDestroy {
+//   products: {title: string}[];
+//   fiteredProducts: any[];
+//   subscription: Subscription;
+//
+//   constructor(private productService: ProductService) {
+//     this.subscription = this.productService.getAll()
+//       .subscribe(products => this.products = this.fiteredProducts = products);
+//   }
+//
+//   filter(query: string) {
+//     this.fiteredProducts = (query) ?
+//       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+//       this.products;
+//   }
+//
+//   ngOnInit() {
+//   }
+//
+//   ngOnDestroy() {
+//     this.subscription.unsubscribe();
+//   }
+//
+// }
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
-})
-export class AdminProductsComponent implements OnInit {
-  products$: Observable<any>;
+  styleUrls: ['./admin-products.component.css'] })
 
-  constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
-  }
+  export class AdminProductsComponent implements OnInit, OnDestroy {
+    products: Product[];
+    filteredProducts: Product[];
+    subscription: Subscription;
 
-  ngOnInit() {
-  }
+    constructor(private productService: ProductService) {
+      this.subscription = this.productService.getAll()
+          .subscribe((products: Product[]) => this.filteredProducts = this.products = products);
+      }
 
-}
+      ngOnInit() {   }
+
+      ngOnDestroy() {
+        this.subscription.unsubscribe();
+      }
+
+      filter(query: string) {
+        this.filteredProducts = (query) ?
+        this.products.filter(p => p['title'].toLowerCase().includes(query.toLowerCase())) :
+        this.products;
+      } }
