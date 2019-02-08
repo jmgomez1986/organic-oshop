b4d85dab402cfa86ba9797155b52c1ecec05cd9d
Matias Gomez <jmgametal@gmail.com>
2019-02-08 15:30:40 -0300

    Agregar items al shopping-cart (a la db de Firebase)

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+import 'rxjs/add/operator/take';
+import { take } from 'rxjs-compat/operator/take';
 
 @Injectable({
   providedIn: 'root'
@@ -18,13 +21,32 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  private async getOrCreateCartId() {
     const cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      const result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    }
-    return this.getCart(cartId);
+    if (cartId) { return cartId; }
+
+    const result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    const cartId = await this.getOrCreateCartId();
+    const item$ = this.getItem(cartId, product.key);
+
+    item$
+      .snapshotChanges()
+      .take(1)
+      .subscribe(item => {
+        item$.update({
+          product: product,
+          quantity:
+            (item.payload.exists() ? item.payload.val().quantity : 0) + 1
+        });
+      });
   }
 }
