08de446760be12dcba4d6d592b4e1998868f307c
Matias Gomez <jmgametal@gmail.com>
2019-02-13 10:05:06 -0300

    Fixing Desing Issue

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,19 +1,21 @@
 import { ShoppingCartItem } from './../models/shopping-cart-item';
 
 export class ShoppingCart {
+  items: ShoppingCartItem[] = [];
 
-  constructor(public items: ShoppingCartItem[]) { }
-
-  get productIds() {
-    return Object.keys(this.items);
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+    // tslint:disable-next-line:forin
+    for (const productId in itemsMap) {
+      this.items.push(itemsMap[productId]); }
+    console.log(this.items);
   }
 
   get totalItemsCount() {
     let count = 0;
 
     // tslint:disable-next-line:forin
-    for ( const productId in this.items ) {
-      count += this.items[productId].quantity;
+    for (const productId in this.itemsMap) {
+      count += this.itemsMap[productId].quantity;
     }
     return count;
   }
