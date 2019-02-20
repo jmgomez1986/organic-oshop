328e6aa6b524a7b2b09eceb1ba2994a923fe7889
Matias Gomez <jmgametal@gmail.com>
2019-02-20 17:39:20 -0300

    Clase 338: Building the Shopping Cart Page (Se crea una pagina con el listado de items del shopping-cart y su cantidad)

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -11,6 +11,10 @@ export class ShoppingCart {
     //   this.items.push(new ShoppingCartItem(item.product, item.quantity)); }
   }
 
+  get productIds() {
+    return Object.keys(this.items);
+  }
+
   get totalItemsCount() {
     let count = 0;
 
