e3df906da858ef4f826f10b397785f13902bee83
Matias Gomez <jmgametal@gmail.com>
2019-02-20 11:55:49 -0300

    Fix: funciona pero no aparecen botones de +/- items

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -21,7 +21,7 @@ export class ShoppingCartService {
   async getCart(): Promise<Observable<ShoppingCart>> {
     const cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId).snapshotChanges()
-      .map((action: any) => {
+      .map(action => {
         const items = action.payload.val().items;
         return new ShoppingCart(items);
       });
