707f36e4d0cf43e768999c46ee2fa3c5525f7889
Matias Gomez <jmgametal@gmail.com>
2019-02-21 11:16:43 -0300

    Clase 341: Refactoring: Extracting ProductQuantityComponents (Se crea un componente para +/- items y se reutiliza para product-card)

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../models/product';
 import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-product-card',
@@ -10,7 +11,7 @@ import { ShoppingCartService } from '../shopping-cart.service';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
-  @Input('shopping-cart') shoppingCart: any;
+  @Input('shopping-cart') shoppingCart: ShoppingCart;
 
   constructor(private cartService: ShoppingCartService) { }
 
@@ -18,13 +19,4 @@ export class ProductCardComponent {
     this.cartService.addToCart(this.product);
   }
 
-  removeFomCart() {
-      this.cartService.removeFromCart(this.product);
-  }
-
-  getQuantity() {
-    if (!this.shoppingCart) { return 0; }
-    const item = this.shoppingCart.items[this.product.key];
-    return item ? item.quantity : 0;
-  }
 }
